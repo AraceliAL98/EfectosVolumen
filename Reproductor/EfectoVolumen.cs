0adb47559932c0183ab999e1599d5c00d72a9f72
Araceli Miyanomori <Araceli Miyanomori@LAPTOP-TV3PHC9Q>
2019-03-04 10:47:31 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor/EfectoVolumen.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Wave;
+
+namespace Reproductor
+{
+    class EfectoVolumen : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var read = fuente.Read(buffer, offset, count);
+
+            for (int i = 0; i < read; i++)
+            {
+                buffer[offset + i] *= 1.0f;
+            }
+            return read;
+        }
+    }
+
+}
